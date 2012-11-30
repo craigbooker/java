3936b792d5c64d5566a887a6a28c4a27e5a142a6
Craig Booker <mail@craigbooker.com>
2012-11-29 18:16:28 -0600

    Week 2 Exercises
    Went through video exercise app.  Created week 2 project.  Add edit text field for range and a label for the edit text field.
    
    Signed-off-by: Craig Booker <mail@craigbooker.com>

---
+++ Java1ProjectWk2/gen/com/craigbooker/java1projectwk2/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.craigbooker.java1projectwk2;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Java1Week2Demo/src/com/craigbooker/ecom/Product.java
@@ -0,0 +1,9 @@
+package com.craigbooker.ecom;
+
+public interface Product {
+public boolean setName(String name);
+public boolean setPrice(double price);
+public String getName();
+public double getPrice();
+
+}
