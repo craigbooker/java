646e86f22bbbfbf34173d18e9accefffee43949d
Craig Booker <mail@craigbooker.com>
2012-12-06 18:46:44 -0600

    After fixing eclipse yet again, I have created another demo project.
    Signed-off-by: Craig Booker <mail@craigbooker.com>

---
+++ Java1DemoWk3/gen/com/craigbooker/java1demowk3/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.craigbooker.java1demowk3;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- Java1ProjectWk3/gen/com/craigbooker/java1projectwk3/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.craigbooker.java1projectwk3;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
