fe16430bd7bddb157a156cccb478e0e8bc46cba4
Craig Booker <mail@craigbooker.com>
2012-12-09 20:16:41 -0600

    Added the basic layout for the search results.
    Signed-off-by: Craig Booker <mail@craigbooker.com>

--- Java1ProjectWk3/src/com/craigbooker/java1projectwk3/SearchResults.java
+++ Java1ProjectWk3/src/com/craigbooker/java1projectwk3/SearchResults.java
@@ -1,5 +1,41 @@
 package com.craigbooker.java1projectwk3;
 
-public class SearchResults {
+import java.util.ArrayList;
 
+import android.content.Context;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+
+public class SearchResults extends LinearLayout {
+
+	Context _context;
+	ListView _listView;
+	ArrayList<String> _results = new ArrayList<String>();
+	
+	public SearchResults(Context context) {
+		super(context);
+		
+		LayoutParams lp;
+		lp = new LayoutParams();
+		_listView = new ListView(_context);
+		_listView.setLayoutParams(lp);
+		
+	}
+	
+	public ListView getResultList() {
+		return _listView;
+	}
+	
+	public void addRow(String ){
+		_results.add(object);
+		_listView.invalidateViews();
+	}
+	
+	public void reset() {
+		_results.clear();
+		_listView.invalidateViews();
+		
+	}
 }
