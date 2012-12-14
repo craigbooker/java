3af9bbaeb16e719dee2bd5502351466f44bf4393
Craig Booker <mail@craigbooker.com>
2012-12-13 22:26:37 -0600

    Adding grid layout framework.
    Signed-off-by: Craig Booker <mail@craigbooker.com>

---
+++ Java1ProjectWk3/src/com/craigbooker/java1projectwk3/PlacesGridDisplay.java
@@ -0,0 +1,20 @@
+package com.craigbooker.java1projectwk3;
+
+import android.content.Context;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class PlacesGridDisplay extends GridLayout {
+
+	
+	
+	public PlacesGridDisplay(Context context){
+		super(context);
+		
+	}
+	
+	
+	
+	
+}
