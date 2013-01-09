d630e1b69b031f16688ab398bfa4fa5389d48cba
Craig Booker <mail@craigbooker.com>
2013-01-09 09:11:44 -0600

    Fixed an issue with display results which made the app work.
    Signed-off-by: Craig Booker <mail@craigbooker.com>

---
+++ J2Project1/src/com/craigbooker/lib/Convert.java
@@ -0,0 +1,31 @@
+package com.craigbooker.lib;
+
+import android.annotation.SuppressLint;
+import java.util.Locale;
+
+public class Convert {
+    public static final double METERS_IN_ONE_MILE = 0.000621371192;
+
+    public static double metersToMiles(double distance) {                   
+            return distance * METERS_IN_ONE_MILE;
+    }
+    
+    public static double milesToMeters(double distance){
+    	return distance / METERS_IN_ONE_MILE; 
+    }
+    
+    @SuppressLint("DefaultLocale")
+	public static String yelpifiedIdString(String id) {
+            StringBuffer sb = new StringBuffer();
+            id = id.trim().toLowerCase(Locale.getDefault());
+            for(int i=0; i<id.length(); i++) {
+                    char c = id.charAt(i);
+                    if((c >= 'a' && c <='z') || (c >= '0' && c <= '9') || c == '-' || c == '_') {
+                            sb.append(c);
+                    } else if(c == ' ') {
+                            sb.append('-');
+                    }
+            }
+            return sb.toString();
+    }
+}
