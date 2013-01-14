b48decf06c9a7338879999a54693de7cfe8f4232
craigbooker <mail@craigbooker.com>
2013-01-14 10:49:20 -0600

    Setup java2 project and mainlistframent file for week 2 of java 2.

--- Java2/src/com/craigbooker/java2/Main.java
+++ Java2/src/com/craigbooker/java2/Main.java
@@ -1,15 +1,31 @@
 package com.craigbooker.java2;
 
+import com.craigbooker.j2project1masterdetail.R;
+import com.craigbooker.j2project1masterdetail.ServiceTypeListFragment;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
 
-public class Main extends Activity {
-
+public class Main extends FragmentActivity implements MainListFragment.Callbacks {
+	
+	private boolean mTwoPane;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		if (findViewById(R.id.servicetype_detail_container) != null) {
+			mTwoPane = true;
+			
+			// In two-pane mode, list items should be given the 'activated' state when touched.
+			((MainListFragment) getSupportFragmentManager()
+								.findFragmentById(R.id.servicetype_list))
+								.setActivateOnItemClick(true);
+		}	
 	}
 
 	@Override
---
+++ Java2/src/com/craigbooker/java2/MainListFragment.java
@@ -0,0 +1,5 @@
+package com.craigbooker.java2;
+
+public class MainListFragment {
+
+}
