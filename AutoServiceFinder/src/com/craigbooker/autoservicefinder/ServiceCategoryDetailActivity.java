c604d47054694cac3c56d8590fe8c5b279341f51
craigbooker <mail@craigbooker.com>
2013-01-16 16:21:38 -0600

    Code cleanup.

--- AutoServiceFinder/src/com/craigbooker/autoservicefinder/ServiceCategoryDetailActivity.java
+++ AutoServiceFinder/src/com/craigbooker/autoservicefinder/ServiceCategoryDetailActivity.java
@@ -7,10 +7,8 @@ import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
 
 /**
- * An activity representing a single ServiceCategory detail screen. This
- * activity is only used on handset devices. On tablet-size devices, item
- * details are presented side-by-side with a list of items in a
- * {@link ServiceCategoryListActivity}.
+ * This activity represents a single ServiceCategory detail screen.  On tablet-size devices, item
+ * details are presented side-by-side with a list of items in a {@link ServiceCategoryListActivity}.
  * <p>
  * This activity is mostly just a 'shell' activity containing nothing more than
  * a {@link ServiceCategoryDetailFragment}.
@@ -24,19 +22,9 @@ public class ServiceCategoryDetailActivity extends FragmentActivity {
 
 		// Show the Up button in the action bar.
 		getActionBar().setDisplayHomeAsUpEnabled(true);
-
-		// savedInstanceState is non-null when there is fragment state
-		// saved from previous configurations of this activity
-		// (e.g. when rotating the screen from portrait to landscape).
-		// In this case, the fragment will automatically be re-added
-		// to its container so we don't need to manually add it.
-		// For more information, see the Fragments API guide at:
-		//
-		// http://developer.android.com/guide/components/fragments.html
-		//
+		
 		if (savedInstanceState == null) {
-			// Create the detail fragment and add it to the activity
-			// using a fragment transaction.
+			// Create the detail fragment and add it to the activity using a fragment transaction.
 			Bundle arguments = new Bundle();
 			arguments.putString(
 					ServiceCategoryDetailFragment.ARG_ITEM_ID,
@@ -54,13 +42,7 @@ public class ServiceCategoryDetailActivity extends FragmentActivity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
+ 
 			NavUtils.navigateUpTo(this, new Intent(this,
 					ServiceCategoryListActivity.class));
 			return true;
