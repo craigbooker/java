f3200568bd272848faeb827abf7e6d7a62ffaddd
craigbooker <mail@craigbooker.com>
2013-01-16 14:25:35 -0600

    Got project setup to pull categories from resources string file.

--- AutoServiceFinder/src/com/craigbooker/autoservicefinder/ServiceCategoryListFragment.java
+++ AutoServiceFinder/src/com/craigbooker/autoservicefinder/ServiceCategoryListFragment.java
@@ -128,13 +128,9 @@ public class ServiceCategoryListFragment extends ListFragment {
 		}
 	}
 
-	/**
-	 * Turns on activate-on-click mode. When this mode is on, list items will be
-	 * given the 'activated' state when touched.
-	 */
+	/* Turns on activate-on-click mode. When this mode is on, list items will be given the 'activated' state when touched.*/
 	public void setActivateOnItemClick(boolean activateOnItemClick) {
-		// When setting CHOICE_MODE_SINGLE, ListView will automatically
-		// give items the 'activated' state when touched.
+		// When setting CHOICE_MODE_SINGLE, ListView will automatically give items the 'activated' state when touched.
 		getListView().setChoiceMode(
 				activateOnItemClick ? ListView.CHOICE_MODE_SINGLE
 						: ListView.CHOICE_MODE_NONE);
