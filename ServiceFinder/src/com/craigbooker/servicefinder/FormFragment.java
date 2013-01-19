4833e251b54caf827c504a17a2f834304ac3ec52
craigbooker <mail@craigbooker.com>
2013-01-18 23:52:54 -0600

    Basic project is setup.  I am adding parts piece-by-piece.

--- ServiceFinder/src/com/craigbooker/servicefinder/FormFragment.java
+++ ServiceFinder/src/com/craigbooker/servicefinder/FormFragment.java
@@ -1,6 +1,12 @@
 package com.craigbooker.servicefinder;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.craigbooker.servicefinder.FormFragment.FormListener;
 
@@ -14,4 +20,16 @@ public class FormFragment extends Fragment {
 		public void onAddToLog();
 		
 	}
-}
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+		super.onCreateView(inflater, container, savedInstanceState);
+		
+		LinearLayout view = (LinearLayout) inflater.inflate(R.layout.form, container, false);
+		
+		Button searchButton = (Button) view.findViewById(R.id.searchTermField);
+		
+		
+		return view;
+	}
+	
+}  //END OF PUBLIC CLASS FORMFRAGMENT
--- ServiceFinder/src/com/craigbooker/servicefinder/MainActivity.java
+++ ServiceFinder/src/com/craigbooker/servicefinder/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends Activity implements FormFragment.FormListener 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.formfrag);
 	}
 
 	@Override
